3ea43448d93d0e190c23a12fe7f6c3234c28cd37
Frank Fishburn <fishburnf@upmc.edu>
2018-07-16 16:34:58 -0400

    Bugfix for TrimBaseline module. Fixes #27

---
+++ +eeg/+util/multimodalfusion.m
@@ -0,0 +1,96 @@
+function [nirsout,eegout]=multimodalfusion(nirsin,eegin);
+
+for idx=1:length(nirsin)
+    
+    ndc=mean(nirsin(idx).data,1);
+    edc=mean(eegin(idx).data,1);
+    
+    nirsin(idx).data=nirsin(idx).data-ones(size(nirsin(idx).data,1),1)*ndc;
+    eegin(idx).data=eegin(idx).data-ones(size(eegin(idx).data,1),1)*edc;
+    
+    nvc=var(nirsin(idx).data,[],1);
+    evc=var(eegin(idx).data,[],1);
+    
+    nirsin(idx).data=nirsin(idx).data./(ones(size(nirsin(idx).data,1),1)*nvc.^.5);
+    eegin(idx).data=eegin(idx).data./(ones(size(eegin(idx).data,1),1)*evc.^.5);
+    
+    
+    [yfilt,f] = nirs.math.innovations(nirsin(idx).data,20,true);
+    [yfilt2,f2] = nirs.math.innovations(eegin(idx).data,20,true);
+    
+    lags=round(eegin(idx).Fs/nirsin(idx).Fs);
+    
+    
+    yfilt(find(nirsin(idx).time>min(nirsin(idx).time(end),eegin(idx).time(end))),:)=[];
+    yfilt2(find(eegin(idx).time>min(nirsin(idx).time(end),eegin(idx).time(end))),:)=[];
+    yfilt2(find(eegin(idx).time<0),:)=[];
+    yfilt2(end+1:ceil(size(yfilt2,1)/lags)*lags,:)=0;
+    X=[];
+    for i=1:lags
+        X=[X yfilt2(i:lags:end,:)];
+    end
+    
+    yfilt(size(X,1)+1:size(yfilt,1),:)=[];
+    
+    n=min(size(yfilt,1),size(X,1));
+    yfilt=yfilt(1:n,:);
+       
+    X=X(1:n,:);
+    
+    [A,B,R,U,V,stats]=canoncorr(yfilt,X);
+    
+    lst=find(stats.p<0.05);
+    
+    if length(lst) < 4
+        lst = [1 2 3];
+    end
+    
+    %U = (X - repmat(mean(X),N,1))*A and
+    % Xhat=U*inv(A)+repmat(mean(X),N,1)
+    %V = (Y - repmat(mean(Y),N,1))*B.
+    % Yhat=V*inv(B)+repmat(mean(Y),N,1)
+    
+    yfilt1hat = U(:,lst)*pinv(A(:,lst))+ones(size(yfilt,1),1)*mean(yfilt,1);
+    Xhat = V(:,lst)*pinv(B(:,lst))+ones(size(X,1),1)*mean(X,1);
+    
+    yfilt2hat=zeros(size(yfilt2));
+    
+    nn=ceil((size(yfilt2hat,1)-1)/lags);
+    Xhat(end+1:nn,:)=0;
+    
+    cnt=0;
+    n=size(yfilt2,2);
+    for i=1:lags
+        yfilt2hat(i:lags:end,:)=Xhat(:,cnt+[1:n]);
+        cnt=cnt+n;
+    end
+    clear y y2;
+    for i=1:length(f)
+        y(:,i)=filter(1,[1; f{i}(2:end)],yfilt1hat(:,i));
+    end
+    for i=1:length(f2)
+        y2(:,i)=filter(1,[1; f2{i}(2:end)],yfilt2hat(:,i));
+    end
+    
+    nirsout(idx)=nirsin(idx);
+    nirsout(idx).data=y;
+    nirsout(idx).time =nirsin(idx).time(find(nirsin(idx).time<=min(nirsin(idx).time(end),eegin(idx).time(end)) & nirsin(idx).time>=0));
+    nirsout(idx).time=nirsout(idx).time(1:size(nirsout(idx).data,1));
+    eegout(idx)=eegin(idx);
+    eegout(idx).data=y2;
+    eegout(idx).time = eegin(idx).time(find(eegin(idx).time<=min(nirsin(idx).time(end),eegin(idx).time(end)) & eegin(idx).time>=0));
+    
+    if(size(eegout(idx).data,1)>length(eegout(idx).time))
+        eegout(idx).data(length(eegout(idx).time)+1:end,:)=[];
+    end
+    eegout(idx).time=eegout(idx).time(1:size(eegout(idx).data,1));
+    
+    nirsout(idx).data=nirsout(idx).data.*(ones(size(nirsout(idx).data,1),1)*nvc.^.5);
+    eegout(idx).data=eegout(idx).data.*(ones(size(eegout(idx).data,1),1)*evc.^.5);
+    
+    nirsout(idx).data=nirsout(idx).data+ones(size(nirsout(idx).data,1),1)*ndc;
+    eegout(idx).data=eegout(idx).data+ones(size(eegout(idx).data,1),1)*edc;
+    
+    %disp (['Finished ' num2str(idx) ' out of ' num2str(length(nirsin))]);
+  
+end
---
+++ +nirs/+modules/Hyperscanning.m
@@ -0,0 +1,252 @@
+classdef Hyperscanning < nirs.modules.AbstractModule
+    %% Hyper-scanning - Computes all-to-all connectivity model between two seperate files
+    % Outputs nirs.core.ConnStats object
+    
+    properties
+        corrfcn;  % function to use to compute correlation (see +nirs/+sFC for options)
+        divide_events;  % if true will parse into multiple conditions
+        min_event_duration;  % minimum duration of events
+        link;
+        symetric;
+        verbose;
+        ignore;  % seconds at start/end of each scan or block to ignore
+        multitrial; % flag to enable computing once per condition rather than averaging over blocks/trials
+    end
+    properties(Hidden=true)
+        % I like to hide options that I don't want the average person
+        % playing with
+        
+        cache_dir;  % (optional) directory to cache results (unset disables caching)
+        cache_rebuild;  % (optional) force rebuild of cached results (don't load previous results from cache, only save new results)
+    end
+    methods
+        function obj = Hyperscanning( prevJob )
+            obj.name = 'Hyperscanning';
+            obj.corrfcn = @(data)nirs.sFC.ar_corr(data,'18xFs',true);  %default to use AR-whitened robust correlation
+            obj.divide_events=false;
+            obj.min_event_duration=30;
+            obj.symetric=true;
+            obj.ignore=10;
+            obj.multitrial=false; 
+            obj.cache_dir='';
+            obj.verbose=false;
+            obj.cache_rebuild=false;
+            
+            obj.link=table(1,1,0,0,'VariableNames',{'ScanA','ScanB','OffsetA','OffsetB'});
+            obj.link(1,:)=[];
+            
+            if nargin > 0
+                obj.prevJob = prevJob;
+            end
+            
+            obj.citation='Santosa, H., Aarabi, A., Perlman, S. B., & Huppert, T. J. (2017). Characterization and correction of the false-discovery rates in resting state connectivity using functional near-infrared spectroscopy. Journal of Biomedical Optics, 22(5), 055002-055002.';
+            
+        end
+        
+        function connStats = runThis( obj, data )
+            
+            
+            if(isempty(obj.link))
+                % NIRx files have a hyperscan variable upon loading that I
+                % can use here
+                if(ismember('hyperscan',nirs.createDemographicsTable(data).Properties.VariableNames))
+                    hyperscanfiles=nirs.createDemographicsTable(data).hyperscan;
+                    
+                    for i=1:length(hyperscanfiles); if(isempty(hyperscanfiles{i})); hyperscanfiles{i}=''; end; end;
+                    uniquefiles=unique(hyperscanfiles);
+                    [ia,ib]=ismember(hyperscanfiles,uniquefiles);
+                    
+                    for i=1:length(uniquefiles)
+                        lst=find(ib==i);
+                        ScanA(i,1)=lst(1);
+                        ScanB(i,1)=lst(2);
+                    end
+                    
+                    OffsetA = zeros(size(ScanA));  % The time shift of the "A" files (in sec)
+                    OffsetB = zeros(size(ScanB));  % The time shift of the "B" files (in sec)
+                    
+                    link = table(ScanA,ScanB,OffsetA,OffsetB);
+                    obj.link=link;
+                else
+                    warning('link variable must be specified');
+                    connStats=nirs.core.sFCStats();
+                    return
+                end
+            end
+            
+            connStats(1:height(obj.link))=nirs.core.sFCStats();
+            
+            for i=1:height(obj.link)
+                
+                idxA = obj.link.ScanA(i);
+                idxB = obj.link.ScanB(i);
+                
+                dataA = data(idxA).data;
+                timeA = data(idxA).time+obj.link.OffsetA(i);
+                dataB = data(idxB).data;
+                timeB = data(idxB).time+obj.link.OffsetB(i);
+                
+                % Make sure we are using the same time base
+                if isequal(timeA,timeB)
+                    time = timeA;
+                else
+                    time=[max(timeA(1),timeB(1)):1/data(idxA).Fs:min(timeA(end),timeB(end))]';
+                    for id=1:size(dataA,2)
+                        dataA(1:length(time),id)=interp1(timeA,dataA(:,id),time);
+                        dataB(1:length(time),id)=interp1(timeB,dataB(:,id),time);
+                    end
+                    dataA=dataA(1:length(time),:);
+                    dataB=dataB(1:length(time),:);
+                end
+                
+                connStats(i).type = obj.corrfcn;
+                connStats(i).description= data(idxA).description;
+                connStats(i).probe = nirs.util.createhyperscanprobe(data(idxA).probe);
+                connStats(i).demographics={data(idxA).demographics; data(idxB).demographics};
+                connStats(i).R=[];
+                
+                if(obj.divide_events)
+                    stim=data(idxA).stimulus;
+                    stim2=data(idxB).stimulus;
+                    
+                    if ~isequal(stim,stim2)
+                        warning('Stimulus events differ between files. Link table may be incorrect.');
+                    end
+                    
+                    cnt=1;
+                    for idx=1:length(stim.keys)
+                        s=stim(stim.keys{idx});
+                        lst=find(s.dur-2*obj.ignore>obj.min_event_duration);
+                        if(length(lst)>0)
+                            s.onset=s.onset(lst);
+                            s.dur=s.dur(lst);
+                            
+                            if(obj.verbose)
+                                disp(['Spliting condition: ' stim.keys{idx}]);
+                            end
+                            n1=size(dataA,2)+size(dataB,2);
+                            r=zeros(n1,n1,length(s.onset));
+                            dfe=zeros(length(s.onset),1);
+                            tmp=nirs.core.Data;
+                            if obj.multitrial % 3-D stack of blocks to calc FC once
+                                dur = min(s.dur) - 2*obj.ignore;
+                                nsamp = floor( dur * data(idxA).Fs );
+                                tmp.data = zeros([nsamp 2*size(dataA,2) length(s.onset)]);
+                                for j=1:length(s.onset)
+                                    if(obj.verbose)
+                                        disp(['   ' num2str(j) ' of ' num2str(length(s.onset))]);
+                                    end
+                                    startpt = find(time>(s.onset(j)+obj.ignore),1,'first');
+                                    lstpts=startpt:min(length(time),startpt+nsamp-1);
+                                    tmp.data(1:length(lstpts),:,j)=[dataA(lstpts,:) dataB(lstpts,:)];
+                                end
+                                tmp.time=time(lstpts);
+                                [r,p,dfe]=obj.corrfcn(tmp);
+                                
+                            else  % Iterate over each block to calc FC
+                                for j=1:length(s.onset)
+                                    if(obj.verbose)
+                                        disp(['   ' num2str(j) ' of ' num2str(length(s.onset))]);
+                                    end
+                                    lstpts=find(time>s.onset(j)+obj.ignore &...
+                                        time<s.onset(j)+s.dur(j)-obj.ignore);
+                                    tmp.data=[dataA(lstpts,:) dataB(lstpts,:)];
+                                    tmp.time=time(lstpts);
+                                    [r(:,:,j),p,dfe(j)]=obj.corrfcn(tmp);
+                                end
+                            end
+                            
+                            if(obj.symetric)
+                                r = atanh(r); % r-to-Z
+                                for j = 1:size(r,3)
+                                    aa=r(1:end/2,1:end/2,j);            % within subject A
+                                    ab=r(1:end/2,end/2+1:end,j);        % from A to B
+                                    ba=r(end/2+1:end,1:end/2,j);        % from B to A
+                                    bb=r(end/2+1:end,end/2+1:end,j);    % within subject B
+                                    r(:,:,j) = ([aa ab; ba bb] + [bb ba; ab aa]) ./ 2;
+                                end
+                                r = tanh(r); % Z-to-r
+                            end
+                                
+                            connStats(i).dfe(cnt)=sum(dfe);
+                            connStats(i).R(:,:,cnt)=tanh(mean(atanh(r),3));
+                            connStats(i).conditions{cnt}=stim.keys{idx};
+                            cnt=cnt+1;
+                            
+                        else
+                            disp(['Skipping condition: ' stim.keys{idx} ...
+                                ': No events > ' num2str(2*obj.ignore+obj.min_event_duration) 's']);
+                            
+                        end
+                        
+                    end
+                    
+                    
+                else
+                    tmp=nirs.core.Data;
+                    tmp.data=[dataA dataB];
+                    tmp.time=time;
+                    
+                    lst=find(tmp.time<obj.ignore | tmp.time>tmp.time(end)-obj.ignore);
+                    tmp.data(lst,:)=[];
+                    tmp.time(lst)=[];
+                    [r,p,dfe]=obj.corrfcn(tmp);
+                    
+                    if(obj.symetric)
+                        r = atanh(r); % r-to-Z
+                        if ~isempty(strfind(func2str(obj.corrfcn),'nirs.sFC.grangers'))
+                            r = exp(2*r); % Z-to-F
+                        end
+                        aa=r(1:end/2,1:end/2);            % within subject A
+                        ab=r(1:end/2,end/2+1:end);        % from A to B
+                        ba=r(end/2+1:end,1:end/2);        % from B to A
+                        bb=r(end/2+1:end,end/2+1:end);    % within subject B
+                        r = ([aa ab; ba bb] + [bb ba; ab aa]) ./ 2;
+                        if ~isempty(strfind(func2str(obj.corrfcn),'nirs.sFC.grangers'))
+                            r = log(r)/2; % F-to-Z
+                        end
+                        r = tanh(r); % Z-to-r
+                    end
+                    
+                    connStats(i).dfe=dfe;
+                    connStats(i).R=r;
+                    connStats(i).conditions=cellstr('rest');
+                end
+                
+                
+                
+                % Compute data hash and load cached result if match is found
+                %                 clear hash cache_file
+                %                 if exist(obj.cache_dir,'dir')
+                %                     hashopt.Method = 'SHA-256';
+                %                     tmpobj = obj;
+                %                     tmpobj.link = []; tmpobj.cache_dir = []; tmpobj.cache_rebuild = [];
+                %                     tmpobj.corrfcn = func2str(tmpobj.corrfcn); tmpobj.prevJob = [];
+                %                     hash = DataHash( {dataA,dataB,tmpobj,mask} , hashopt );
+                %                     cache_file = fullfile( obj.cache_dir , [hash '.mat'] );
+                %                     if ~obj.cache_rebuild && exist(cache_file,'file')
+                %                         tmp = load(cache_file,'connStats');
+                %                         connStats(i).R = tmp.connStats.R;
+                %                         connStats(i).dfe = tmp.connStats.dfe;
+                %                         disp(['Finished ' num2str(i) ' of ' num2str(height(obj.link)) ' (cached)'])
+                %                         continue;
+                %                     end
+                %                 end
+                
+                connStats(i).R(1:end/2,1:end/2,:)=0;
+                connStats(i).R(1+end/2:end,1+end/2:end,:)=0;
+                
+                disp(['Finished ' num2str(i) ' of ' num2str(height(obj.link))])
+                
+                %                 if exist('cache_file','var')
+                %                     tmp = [];
+                %                     tmp.connStats = connStats(i);
+                %                     save(cache_file,'-struct','tmp');
+                %                 end
+                
+            end
+            
+        end
+    end
+end
+
